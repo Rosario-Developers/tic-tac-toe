27979901f670c254f78129e62e7c11df749250b1
Luca Seri <lucaseri2003@gmail.com>
2022-10-08 21:03:14 -0300

    feat: add visualization of tokens

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 const squares = document.querySelectorAll('.square');
-const board = document.querySelector('.game');
-
-const handleClick = (event) => {
-  const square = event.target;
-  const position = square?.id;
-  if (position) {
-    square.innerHTML = 'X';
-  }
-};
-
-board.addEventListener('click', handleClick);
-
-const X = 'X';
-const O = 'O';
+const board = document.querySelector('.board');
 
 class Game {
   constructor() {
@@ -21,16 +8,32 @@ class Game {
       ['', '', ''],
       ['', '', ''],
     ];
-    this.token = 'x';
+    this.currentPlayer = 'X';
   }
 
   handleClick(event) {
     const square = event.target;
     const position = square?.id;
-    if (position) {
-      square.innerHTML = 'X';
+    if (position && !this.isClicked(square)) {
+      square.innerHTML = this.currentPlayer;
+      square.classList.add(this.currentPlayer === 'X' ? 'cross' : 'circle');
+      this.togglePlayer()
     }
+  }
+
+  togglePlayer() {
+    this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  }
+
+  isClicked(square) {
+    return square.innerHTML !== '';
+  }
+
+  checkWinner() {
+
   }
 }
 
 const game = new Game();
+
+board.addEventListener('click', game.handleClick.bind(game));
