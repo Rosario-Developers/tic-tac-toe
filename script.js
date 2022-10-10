6f3d74bf7ffd42937edcc3f95e32a2a98a396d61
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-10-10 13:55:25 -0300

    feat: finish game logic
    Co-authored-by: NicolasTomas <NicolasTomas@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const squares = document.querySelectorAll('.square');
 const board = document.querySelector('.board');
+const turn = document.getElementById('turn');
 
 class Game {
   constructor() {
@@ -13,11 +14,13 @@ class Game {
 
   handleClick(event) {
     const square = event.target;
-    const position = square?.id;
-    if (position && !this.isClicked(square)) {
+    const [positionX, positionY] = square?.id.split('');
+    if (positionX && !this.isClicked(square)) {
       square.innerHTML = this.currentPlayer;
       square.classList.add(this.currentPlayer === 'X' ? 'cross' : 'circle');
-      this.togglePlayer()
+      this.board[positionX][positionY] = this.currentPlayer;
+      this.checkWinner();
+      this.togglePlayer();
     }
   }
 
@@ -26,14 +29,67 @@ class Game {
   }
 
   isClicked(square) {
-    return square.innerHTML !== '';
+    return square.innerHTML;
+  }
+
+  checkRows() {
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.board[i][0] === this.board[i][1] &&
+        this.board[i][1] === this.board[i][2] &&
+        this.board[i][0]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkColumns() {
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.board[0][i] === this.board[1][i] &&
+        this.board[1][i] === this.board[2][i] &&
+        this.board[0][i]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkDiagonals() {
+    if (
+      this.board[0][0] === this.board[1][1] &&
+      this.board[1][1] === this.board[2][2] &&
+      this.board[0][0]
+    ) {
+      return true;
+    }
+    if (
+      this.board[0][2] === this.board[1][1] &&
+      this.board[1][1] === this.board[2][0] &&
+      this.board[0][2] !== ''
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  showWinner() {
+    alert(`${this.currentPlayer} Wins!`);
   }
 
   checkWinner() {
-
+    const winner =
+      this.checkRows() || this.checkColumns() || this.checkDiagonals();
+    if (winner) {
+      this.showWinner(winner);
+    }
   }
 }
 
 const game = new Game();
 
 board.addEventListener('click', game.handleClick.bind(game));
+turn.innerHTML = `${game.currentPlayer} turn`;
